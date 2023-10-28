5f93862b41da617aad9c0f50b95ddc55e7c66334
Geethapriya261 <geethapriya.n@gavstech.com>
2023-10-28 16:23:00 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react';
+ 
+import axios from 'axios';
+ 
+ 
+ 
 function App() {
+ 
+  const [data, setData] = useState([]);
+ 
+  const [userid, setUserId] = useState('');
+ 
+  const [password, setPassword] = useState('');
+ 
+  const [email, setEmail] = useState('');
+ 
+ 
+ 
+  const updateUserId = (event) => {setUserId(event.target.value);};
+ 
+ 
+ 
+  const updatePassword = (event) => {setPassword(event.target.value);};
+ 
+ 
+ 
+  const updateEmail = (event) => {setEmail(event.target.value);};
+ 
+ 
+ 
+  const insertUsers = (event) => {
+ 
+    event.preventDefault();
+ 
+    axios.post('http://localhost:3001/insert', { userid: userid, emailid: email, password: password })
+ 
+      .then((res) => {console.log(res);
+ 
+        fetchData();
+ 
+      });
+ 
+  };
+ 
+ 
+ 
+  const updateUsers = () => {
+ 
+    axios.put('http://localhost:3001/update', { userid: userid, emailid: email , password: password})
+ 
+      .then((res) => {
+ 
+        console.log(res);
+ 
+        fetchData();
+ 
+      });
+ 
+  };
+ 
+ 
+ 
+  const deleteUsers = () => {
+ 
+    axios.delete(`http://localhost:3001/delete?userid=${userid}`)
+ 
+      .then((res) => {
+ 
+        console.log(res);
+ 
+        fetchData();
+ 
+      });
+ 
+  };
+ 
+ 
+ 
+  const fetchData = () => {
+ 
+    fetch('http://localhost:3001/getAll')
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  };
+ 
+ 
+ 
+  useEffect(() => {
+ 
+    fetchData();
+ 
+  }, []);
+ 
+ 
+ 
   return (
+ 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ 
+      <center>
+ 
+        <form onSubmit={insertUsers}>
+ 
+          <b>User ID</b>
+ 
+          <input type="text" value={userid} onChange={updateUserId} /><br />
+ 
+          <b>Email ID</b>
+ 
+          <input type="email" value={email} onChange={updateEmail} /><br />
+          <b>Password</b>
+          <input type="password" value={password} onChange={updatePassword} /><br />
 
+ 
+          <input type="submit" value="Add" />&nbsp;&nbsp;
+ 
+          <input type="reset" value="Reset" />&nbsp;&nbsp;
+ 
+          <input type="button" value="Update" onClick={updateUsers} />&nbsp;&nbsp;
+ 
+          <input type="button" value="Delete" onClick={deleteUsers} />&nbsp;&nbsp;
+ 
+        </form>
+ 
+      </center>
+ 
+ 
+ 
+      <ul>
+ 
+        {data.map((item) => (
+ 
+          <li key={item.userid}>
+ 
+            {item.userid},  {item.emailid},{item.password}
+ 
+          </li>
+ 
+        ))}
+ 
+      </ul>
+ 
+    </div>
+ 
+  );
+ 
+}
+ 
+ 
+ 
 export default App;
